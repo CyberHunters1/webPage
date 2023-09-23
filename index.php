eafbec9f0929380afaa15952d26d78a46315f50b
Aldo <aldobarrios72@aragon.unam.mx>
2023-09-23 14:59:17 -0600

    Iclusión de front

--- index.php
+++ index.php
@@ -7,41 +7,68 @@ if (!empty($_SESSION['active'])) {
 ?>
 
 
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>Inventario Graph</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="src/css/Styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900"
+    rel="stylesheet" />
+  <title>CyberHunters</title>
+  <!-- Bootstrap core CSS -->
+  <link href="./src/css/bootstrap.min.css" rel="stylesheet" />
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="./src/css/fontawesome.css" />
+  <link rel="stylesheet" href="./src/css/templatemo-grad-school.css" />
+  <link rel="stylesheet" href="./src/css/owl.css" />
+  <link rel="stylesheet" href="./src/css/lightbox.css" />
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <link rel="shortcut icon" href="./src/images/cyberhunter_logo.png" />
 
 </head>
 
 <body>
-    <div>
-        <div>
-            <h1 style="text-align:center;">Sistema de Inventario ING-GRAPH</h1>
-        </div>
-        <div>
-            <div>
-                <h1>Iniciar Sesión</h1>
-            </div>
-            <form action="func.php" method="POST">
 
-                <div class="form-group">
-                    <label for="usuario">Usuario</label>
-                    <input type="text" placeholder="Usuario" name="usuario" id="usuario">
-                </div>
-                <div class="form-group">
-                    <label for="clave">Contraseña</label>
-                    <input type="password" placeholder="Contraseña" name="clave" id="clave">
-                </div>
-                <input type="submit" value="Iniciar">
-            </form>
-        </div>
+  <header class="main-header clearfix" role="header">
+    <div class="logo">
+      <a href="#"><em>Cyber</em>Hunt<em>ers</em></a>
     </div>
+  </header>
+  <main>
+    <div class="Formulario p-4">
+      <h1>Inicio de Sesión </h1>
+      <form action="func.php" method="POST">
+        <div class="Username">
+          <input type="text" placeholder="Usuario" name="usuario" id="usuario">
+          
+        </div>
+        <div class="Username">
+          <input type="password" placeholder="Contraseña" name="clave" id="clave">
+
+        </div>
+        <input type="submit" value="Iniciar">
+      </form>
+    </div>
+  </main>
+
+  <footer>
+    <div class="pie-de-pagina">
+      <div class="col-md-12">
+        <p>
+          <i class="fa fa-copyright"></i> Copyright 2023 by Cyberhunters |
+          Diseño:
+          <a href="https://templatemo.com" rel="sponsored" target="_parent">Cyberhunters</a>
+        </p>
+      </div>
+    </div>
+    </div>
+  </footer>
+
+
 </body>
 
 </html>
